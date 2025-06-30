09097a3cec36a4c652e30db9680d4c76cb321089
Kenan <ktasdemiriletisim@gmail.com>
2025-07-01 01:10:48 +0300

    Initial Commit

---
+++ tiktokuiapp/tiktokuiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiktokuiappApp.swift
+//  tiktokuiapp
+//
+//  Created by kenan on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct tiktokuiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tiktokuiappTests/tiktokuiappTests.swift
@@ -0,0 +1,17 @@
+//
+//  tiktokuiappTests.swift
+//  tiktokuiappTests
+//
+//  Created by kenan on 7/1/25.
+//
+
+import Testing
+@testable import tiktokuiapp
+
+struct tiktokuiappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
